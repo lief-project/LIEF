880a48caeee6acb892a693f4fe8b13ac89e97359
Zhang <admin@mayuyu.io>
2017-04-07 18:41:51 +0100

    Formatting

--- src/MachO/EncryptionInfoCommand.cpp
+++ src/MachO/EncryptionInfoCommand.cpp
@@ -14,85 +14,78 @@
  * limitations under the License.
  */
 #include "LIEF/visitors/Hash.hpp"
-
 #include "LIEF/MachO/EncryptionInfoCommand.hpp"
 
 namespace LIEF {
 namespace MachO {
-
 EncryptionInfoCommand::EncryptionInfoCommand(void) = default;
-EncryptionInfoCommand& EncryptionInfoCommand::operator=(const EncryptionInfoCommand&) = default;
-EncryptionInfoCommand::EncryptionInfoCommand(const EncryptionInfoCommand&) = default;
+EncryptionInfoCommand & EncryptionInfoCommand::operator = (const EncryptionInfoCommand & ) = default;
+EncryptionInfoCommand::EncryptionInfoCommand(const EncryptionInfoCommand & ) = default;
 EncryptionInfoCommand::~EncryptionInfoCommand(void) = default;
-
-EncryptionInfoCommand::EncryptionInfoCommand(const encryption_info_command_32 *command) :
-        crypt_offset_{command->cryptoff},
-        crypt_size_{command->cryptsize},
-        crypt_id_{command->cryptid}
+EncryptionInfoCommand::EncryptionInfoCommand(const encryption_info_command_32 * command):
+    crypt_offset_ {command->cryptoff},
+    crypt_size_ {command->cryptsize},
+    crypt_id_ {command->cryptid}
 {
-        this->command_ = static_cast<LOAD_COMMAND_TYPES>(command->cmd);
-        this->size_    = command->cmdsize;
-        this->pad_=0;
+    this->command_ = static_cast < LOAD_COMMAND_TYPES > (command->cmd);
+    this->size_ = command->cmdsize;
+    this->pad_ = 0;
 }
-EncryptionInfoCommand::EncryptionInfoCommand(const encryption_info_command_64 *command) :
-        crypt_offset_{command->cryptoff},
-        crypt_size_{command->cryptsize},
-        crypt_id_{command->cryptid},
-        pad_{command->pad}
+EncryptionInfoCommand::EncryptionInfoCommand(const encryption_info_command_64 * command):
+    crypt_offset_ {command->cryptoff},
+    crypt_size_ {command->cryptsize},
+    crypt_id_ {command->cryptid},
+    pad_ {command->pad}
 {
-        this->command_ = static_cast<LOAD_COMMAND_TYPES>(command->cmd);
-        this->size_    = command->cmdsize;
+    this->command_ = static_cast < LOAD_COMMAND_TYPES > (command->cmd);
+    this->size_ = command->cmdsize;
 }
 
-void EncryptionInfoCommand::accept(Visitor& visitor) const {
-        LoadCommand::accept(visitor);
-        visitor.visit(this->crypt_id());
-        visitor.visit(this->crypt_size());
-        visitor.visit(this->crypt_offset());
+void EncryptionInfoCommand::accept(Visitor & visitor) const {
+    LoadCommand::accept(visitor);
+    visitor.visit(this->crypt_id());
+    visitor.visit(this->crypt_size());
+    visitor.visit(this->crypt_offset());
 }
 uint32_t EncryptionInfoCommand::crypt_offset(void) const {
-        return this->crypt_offset_;
+    return this->crypt_offset_;
 }
 uint32_t EncryptionInfoCommand::crypt_size(void) const {
-        return this->crypt_size_;
+    return this->crypt_size_;
 }
 uint32_t EncryptionInfoCommand::crypt_id(void) const {
-        return this->crypt_id_;
+    return this->crypt_id_;
 }
-uint32_t EncryptionInfoCommand::pad(void) const{
-        return this->pad_;
+uint32_t EncryptionInfoCommand::pad(void) const {
+    return this->pad_;
 }
-void EncryptionInfoCommand::crypt_offset(uint32_t offset){
-        this->crypt_offset_ = offset;
+void EncryptionInfoCommand::crypt_offset(uint32_t offset) {
+    this->crypt_offset_ = offset;
 }
-void EncryptionInfoCommand::crypt_size(uint32_t sz){
-        this->crypt_size_ = sz;
+void EncryptionInfoCommand::crypt_size(uint32_t sz) {
+    this->crypt_size_ = sz;
 }
-void EncryptionInfoCommand::crypt_id(uint32_t id){
-        this->crypt_id_ = id;
+void EncryptionInfoCommand::crypt_id(uint32_t id) {
+    this->crypt_id_ = id;
 }
-void EncryptionInfoCommand::pad(uint32_t pd){
-        this->pad_=pd;
+void EncryptionInfoCommand::pad(uint32_t pd) {
+    this->pad_ = pd;
 }
-
-bool EncryptionInfoCommand::operator==(const EncryptionInfoCommand& rhs) const {
-        size_t hash_lhs = Hash::hash(*this);
-        size_t hash_rhs = Hash::hash(rhs);
-        return hash_lhs == hash_rhs;
+bool EncryptionInfoCommand::operator == (const EncryptionInfoCommand & rhs) const {
+    size_t hash_lhs = Hash::hash( * this);
+    size_t hash_rhs = Hash::hash(rhs);
+    return hash_lhs == hash_rhs;
 }
-
-bool EncryptionInfoCommand::operator!=(const EncryptionInfoCommand& rhs) const {
-        return not (*this == rhs);
+bool EncryptionInfoCommand::operator != (const EncryptionInfoCommand & rhs) const {
+    return not( * this == rhs);
 }
-
-std::ostream& EncryptionInfoCommand::print(std::ostream& os) const {
-        LoadCommand::print(os);
-        os << "CryptID    : " << this->crypt_id() << std::endl;
-        os << "CryptSize    : " << this->crypt_size() << std::endl;
-        os << "CryptOffset    : " << this->crypt_offset() << std::endl;
-        return os;
+std::ostream & EncryptionInfoCommand::print(std::ostream & os) const {
+    LoadCommand::print(os);
+    os << "CryptID    : " << this->crypt_id() << std::endl;
+    os << "CryptSize    : " << this->crypt_size() << std::endl;
+    os << "CryptOffset    : " << this->crypt_offset() << std::endl;
+    return os;
 }
 
-
-}
-}
+}//namespace MachO
+}//namesapce LIEF
