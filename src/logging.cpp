d072780da0872d4be1588179cfde021b6cc29d3f
Alberto Garcia Illera <agarciaillera@gmail.com>
2017-11-20 01:13:44 +0700

    change include order

--- src/logging.cpp
+++ src/logging.cpp
@@ -13,9 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "LIEF/logging++.h"
+
 #include "LIEF/config.h"
 #include "LIEF/logging.hpp"
+#include "LIEF/logging++.h"
 #include <map>
 
 #if defined(LIEF_LOGGING_SUPPORT)
