27666e4f02569f53d03a35a6fdd886595af12f1e
Romain Thomas <rthomas@quarkslab.com>
2019-09-09 07:44:59 +0200

    Add Android x86/x86-64 cmake scripts

---
+++ scripts/cmake-config/lief-android-x86.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+cmake ..                                \
+  -DLIEF_DOC=off                        \
+  -DLIEF_PYTHON_API=off                 \
+  -DLIEF_EXAMPLES=off                   \
+  -DLIEF_C_API=off                      \
+  -DLIEF_PE=off                         \
+  -DLIEF_MACHO=off                      \
+  -DLIEF_OAT=off                        \
+  -DLIEF_DEX=off                        \
+  -DLIEF_VDEX=off                       \
+  -DLIEF_ART=off                        \
+  -DLIEF_LOGGING=off                    \
+  -DANDROID_ABI="x86"           \
+  -DANDROID_PLATFORM=android-24         \
+  -DCMAKE_INSTALL_PREFIX=$(pwd)/install \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo     \
+  -DCMAKE_TOOLCHAIN_FILE=${ANDROID_SDK}/ndk-bundle/build/cmake/android.toolchain.cmake
