fce157b45f5207267849a3d8dfe06824496d7484
Romain Thomas <me@romainthomas.fr>
2021-02-10 19:37:51 +0100

    Fix related to #540

--- cmake/cpack.config.cmake
+++ cmake/cpack.config.cmake
@@ -1,6 +1,6 @@
 include("static-release/CPackConfig.cmake")
 
 set(CPACK_INSTALL_CMAKE_PROJECTS ${CPACK_INSTALL_CMAKE_PROJECTS}
-  static-release LIEF ALL /
   shared-release LIEF ALL /
+  static-release LIEF ALL /
 )
