77328b0071c7254567d9e8383ea0f5f876f9da17
Romain Thomas <me@romainthomas.fr>
2022-02-08 21:16:25 +0100

    Bump year

--- include/LIEF/PE/signature/attributes.hpp
+++ include/LIEF/PE/signature/attributes.hpp
@@ -1,5 +1,5 @@
-/* Copyright 2021 R. Thomas
- * Copyright 2021 Quarkslab
+/* Copyright 2021 - 2022 R. Thomas
+ * Copyright 2021 - 2022 Quarkslab
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
