20cde42dc3b9368c991bc828127756e8d82ce7c6
Romain Thomas <me@romainthomas.fr>
2022-08-13 04:42:33 +0200

    Resolve #767

--- src/MachO/ThreadCommand.cpp
+++ src/MachO/ThreadCommand.cpp
@@ -77,24 +77,36 @@ uint64_t ThreadCommand::pc() const {
   switch(architecture_) {
     case CPU_TYPES::CPU_TYPE_X86:
       {
+        if (state_.size() < sizeof(details::x86_thread_state_t)) {
+          return entry;
+        }
         entry = reinterpret_cast<const details::x86_thread_state_t*>(state_.data())->eip;
         break;
       }
 
     case CPU_TYPES::CPU_TYPE_X86_64:
       {
+        if (state_.size() < sizeof(details::x86_thread_state64_t)) {
+          return entry;
+        }
         entry = reinterpret_cast<const details::x86_thread_state64_t*>(state_.data())->rip;
         break;
       }
 
     case CPU_TYPES::CPU_TYPE_ARM:
       {
+        if (state_.size() < sizeof(details::arm_thread_state_t)) {
+          return entry;
+        }
         entry = reinterpret_cast<const details::arm_thread_state_t*>(state_.data())->r15;
         break;
       }
 
     case CPU_TYPES::CPU_TYPE_ARM64:
       {
+        if (state_.size() < sizeof(details::arm_thread_state64_t)) {
+          return entry;
+        }
         entry = reinterpret_cast<const details::arm_thread_state64_t*>(state_.data())->pc;
         break;
       }
