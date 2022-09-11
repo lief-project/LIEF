fad1db9e8b82438b33ce37ce531e5a3e16ab8114
Romain Thomas <me@romainthomas.fr>
2022-09-11 16:23:49 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -457,7 +457,7 @@ def get_pkg_info_version(pkg_info_file):
 
 
 def get_version() -> str:
-    version   = "0.12.1"
+    version   = "0.12.2"
     pkg_info  = os.path.join(CURRENT_DIR, "{}.egg-info".format(PACKAGE_NAME), "PKG-INFO")
     git_dir   = os.path.join(CURRENT_DIR, ".git")
     if os.path.isdir(git_dir):
