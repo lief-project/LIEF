24935f654f6df700a9a062298258b9485f584502
Romain Thomas <me@romainthomas.fr>
2022-09-12 07:10:18 +0200

    Fix #784

--- src/MachO/Binary.cpp
+++ src/MachO/Binary.cpp
@@ -540,7 +540,7 @@ const SegmentCommand* Binary::segment_from_offset(uint64_t offset) const {
   }
 
   auto it = offset_seg_.lower_bound(offset);
-  if (it->first == offset || it == it_begin) {
+  if (it != std::end(offset_seg_) && (it->first == offset || it == it_begin)) {
     SegmentCommand* seg = it->second;
     if (seg->file_offset() <= offset && offset < (seg->file_offset() + seg->file_size())) {
       return seg;
