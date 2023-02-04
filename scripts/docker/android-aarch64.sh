6986f4998c0e5211743a1f8a9c46a5fcc0c858fc
Romain Thomas <me@romainthomas.fr>
2023-02-04 16:06:09 +0100

    Fix linker flags

--- scripts/docker/android-aarch64.sh
+++ scripts/docker/android-aarch64.sh
@@ -12,7 +12,7 @@ pushd build/$ARCH_DIR/shared-release
 cmake ../../.. -GNinja \
   -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
   -DCMAKE_C_FLAGS="$CFLAGS" \
-  -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-Bdynamic -llog -Wl,-Bstatic" \
+  -DCMAKE_SHARED_LINKER_FLAGS="-static-libstdc++" \
   -DCMAKE_LINK_WHAT_YOU_USE=on \
   -DBUILD_SHARED_LIBS=on \
   -DLIEF_PYTHON_API=off \
@@ -27,7 +27,7 @@ pushd build/$ARCH_DIR/static-release
 cmake ../../.. -GNinja \
   -DCMAKE_LINK_WHAT_YOU_USE=on \
   -DBUILD_SHARED_LIBS=off \
-  -DCMAKE_EXE_LINKER_FLAGS="-Wl,-Bdynamic -llog -lc -Wl,-Bstatic" \
+  -DCMAKE_EXE_LINKER_FLAGS="-static-libstdc++" \
   -DLIEF_PYTHON_API=off \
   -DLIEF_INSTALL_COMPILED_EXAMPLES=on \
   -DCMAKE_BUILD_TYPE=Release
