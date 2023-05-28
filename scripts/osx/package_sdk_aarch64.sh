e148d9ebd6bc02382f86a16cf8604cb891a6c28b
Romain Thomas <me@romainthomas.fr>
2023-05-28 09:25:43 +0200

    Add OSX arm64 workflow

--- scripts/osx/package_sdk_aarch64.sh
+++ scripts/osx/package_sdk_aarch64.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/sh
 set -ex
 
-mkdir -p build/osx-aarch64/static-release && mkdir -p build/osx-aarch64/shared-release
+mkdir -p build/osx-aarch64/static-release && \
+mkdir -p build/osx-aarch64/shared-release
+
 pushd build/osx-aarch64/shared-release
 
-cmake ../../.. -GNinja \
-  -DBUILD_SHARED_LIBS=on \
-  -DLIEF_PYTHON_API=off \
+cmake ../../.. -GNinja                 \
+  -DCMAKE_OSX_ARCHITECTURES=arm64      \
+  -DBUILD_SHARED_LIBS=on               \
+  -DLIEF_PYTHON_API=off                \
   -DLIEF_INSTALL_COMPILED_EXAMPLES=off \
   -DCMAKE_BUILD_TYPE=Release
 
@@ -16,9 +19,10 @@ ninja
 popd
 pushd build/osx-aarch64/static-release
 
-cmake ../../.. -GNinja \
-  -DBUILD_SHARED_LIBS=off \
-  -DLIEF_PYTHON_API=off \
+cmake ../../.. -GNinja                \
+  -DCMAKE_OSX_ARCHITECTURES=arm64     \
+  -DBUILD_SHARED_LIBS=off             \
+  -DLIEF_PYTHON_API=off               \
   -DLIEF_INSTALL_COMPILED_EXAMPLES=on \
   -DCMAKE_BUILD_TYPE=Release
 
