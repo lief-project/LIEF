aa45e2ed71f23133192d2be89ded1235dc371566
Romain Thomas <me@romainthomas.fr>
2023-06-04 09:07:15 +0200

    Add tests for LoongArch (PR: 921)

---
+++ tests/elf/test_loongarch.py
@@ -0,0 +1,11 @@
+import lief
+from utils import get_sample, is_64bits_platform
+
+def test_loongarch():
+    elf = lief.ELF.parse(get_sample('ELF/art_reader.loongarch'))
+
+    assert elf.header.machine_type == lief.ELF.ARCH.LOONGARCH
+
+    relocation = elf.relocations[0]
+    assert relocation.size == 26
+    assert relocation.type == int(lief.ELF.RELOCATION_LOONGARCH.LARCH_B26)
