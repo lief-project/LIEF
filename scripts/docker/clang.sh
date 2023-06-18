f6f8c749f21493b35efc1e5920f791c6aa0c79d5
Romain Thomas <me@romainthomas.fr>
2023-06-18 09:01:18 +0200

    Update to new version

--- scripts/docker/clang.sh
+++ scripts/docker/clang.sh
@@ -2,5 +2,5 @@
 set -ex
 cmake -B ${BUILD_DIR} -S ${SRC_DIR} -GNinja \
       -DLIEF_USE_CCACHE=off \
-      -DCMAKE_CXX_COMPILER=/usr/bin/clang++-13 -DCMAKE_C_COMPILER=/usr/bin/clang-13
+      -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_C_COMPILER=/usr/bin/clang
 ninja -C ${BUILD_DIR} LIB_LIEF
--- scripts/docker/gcc.sh
+++ scripts/docker/gcc.sh
@@ -2,5 +2,5 @@
 set -ex
 cmake -B ${BUILD_DIR} -S ${SRC_DIR} -GNinja \
       -DLIEF_USE_CCACHE=off \
-      -DCMAKE_CXX_COMPILER=/usr/bin/g++-11 -DCMAKE_C_COMPILER=/usr/bin/gcc-11
+      -DCMAKE_CXX_COMPILER=/usr/bin/g++ -DCMAKE_C_COMPILER=/usr/bin/gcc
 ninja -C ${BUILD_DIR} LIB_LIEF
