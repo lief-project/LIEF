e8be94770c650fbc8c1c061d42d10553c0bcf981
Romain Thomas <me@romainthomas.fr>
2023-10-07 06:26:35 +0200

    Fix temp path

--- cmake/LIEFApi.cmake
+++ cmake/LIEFApi.cmake
@@ -6,11 +6,6 @@ set(__add_lief_api ON)
 # Python
 # ------
 if (LIEF_PYTHON_API)
-  if(WIN32)
-    set(PYTHON_BUILD_LIEF_DIRECTORY "${CMAKE_BINARY_DIR}/api/python/Release")
-  else()
-    set(PYTHON_BUILD_LIEF_DIRECTORY "${CMAKE_BINARY_DIR}/api/python")
-  endif()
   add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/api/python")
 endif()
 
@@ -18,7 +13,7 @@ endif()
 # -----
 if(LIEF_C_API)
   target_include_directories(LIB_LIEF
-    PUBLIC  "$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/api/c/include>")
+    PUBLIC "$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/api/c/include>")
 
   include("${CMAKE_CURRENT_SOURCE_DIR}/api/c/CMakeLists.txt")
 endif()
