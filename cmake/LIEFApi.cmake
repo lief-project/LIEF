b5b845a0763d5da81dfb181b55aa045fb54bf13b
Romain Thomas <me@romainthomas.fr>
2023-10-07 06:45:00 +0200

    Cleanup cmake

--- cmake/LIEFApi.cmake
+++ cmake/LIEFApi.cmake
@@ -6,15 +6,15 @@ set(__add_lief_api ON)
 # Python
 # ------
 if (LIEF_PYTHON_API)
-  add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/api/python")
+  add_subdirectory(api/python)
 endif()
 
 # C API
 # -----
 if(LIEF_C_API)
-  target_include_directories(LIB_LIEF
-    PUBLIC "$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/api/c/include>")
+  target_include_directories(LIB_LIEF PUBLIC
+    "$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/api/c/include>")
 
-  include("${CMAKE_CURRENT_SOURCE_DIR}/api/c/CMakeLists.txt")
+  add_subdirectory(api/c)
 endif()
 
