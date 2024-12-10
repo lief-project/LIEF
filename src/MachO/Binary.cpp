b78bf38d9ba454dc16f5226a2416595d6623b24f
Uldis Kalnins <ukalnins@whitecryption.com>
2024-12-10 14:43:43 +0200

    Fix symbols shift in MachO::Binary::shift_command
    Fix adjustments of symbols: their values should be compared
    to VA instead of file offset.
    
    Co-authored-by: Richard Dzenis <rdzenis@whitecryption.com>

--- src/MachO/Binary.cpp
+++ src/MachO/Binary.cpp
@@ -640,9 +640,8 @@ void Binary::shift_command(size_t width, uint64_t from_offset) {
 
     for (std::unique_ptr<Symbol>& s : symbols_) {
       if (s->type() == Symbol::TYPE::SECTION) {
-        uint64_t value = s->value();
-        if (value > from_offset) {
-          s->value(value + width);
+        if (s->value() > virtual_address) {
+          s->value(s->value() + width);
         }
       }
     }
