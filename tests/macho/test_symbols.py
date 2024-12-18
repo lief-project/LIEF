67a338e6f7cb6ce31c3d0f599e485fc4c60b1431
Richard Dzenis <rdzenis@whitecryption.com>
2024-12-18 18:28:28 +0200

    Add macho/test_chained_exports_after_shift
    This test checks that exports in dyld_exports_trie
    are shifted correctly.

--- tests/macho/test_symbols.py
+++ tests/macho/test_symbols.py
@@ -166,3 +166,21 @@ def test_exports_after_shift(tmp_path):
     macho.shift(shift)
     new_exports_addrs = [e.address for e in macho.dyld_info.exports]
     assert new_exports_addrs == expected_exports_addrs
+
+def test_chained_exports_after_shift(tmp_path):
+    bin_path = pathlib.Path(get_sample("MachO/MachO64_AArch64_weak-sym-fc.bin"))
+    macho = lief.MachO.parse(bin_path.as_posix()).at(0)
+    assert macho.dyld_exports_trie
+
+    shift = 0x10000
+    loadcommands_end = 32 + macho.header.sizeof_cmds # sizeof(mach_header_64) + size of load command table
+    def get_shifted_export(e):
+        address = e.address
+        if address > loadcommands_end:
+            address += shift
+        return address
+
+    expected_exports_addrs = [get_shifted_export(e) for e in macho.dyld_exports_trie.exports]
+    macho.shift(shift)
+    new_exports_addrs = [e.address for e in macho.dyld_exports_trie.exports]
+    assert new_exports_addrs == expected_exports_addrs
