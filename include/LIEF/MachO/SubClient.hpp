2c0bf31cc0eba82db7e132e12107cf00d1a4fb25
Romain Thomas <me@romainthomas.fr>
2025-01-01 09:49:21 +0100

    ++year

--- include/LIEF/MachO/SubClient.hpp
+++ include/LIEF/MachO/SubClient.hpp
@@ -1,4 +1,4 @@
-/* Copyright 2017 - 2024 R. Thomas
+/* Copyright 2017 - 2025 R. Thomas
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
