c1489422bd856469e682ca01dee1dcd290600af7
Romain Thomas <me@romainthomas.fr>
2025-02-22 04:16:15 +0100

    Update ELF fuzzer

--- fuzzing/elf_fuzzer.cpp
+++ fuzzing/elf_fuzzer.cpp
@@ -1,11 +1,11 @@
 #include <LIEF/LIEF.hpp>
-#include <vector>
+#include <LIEF/BinaryStream/SpanStream.hpp>
 #include <memory>
 #include <sstream>
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-  std::vector<uint8_t> raw = {data, data + size};
-  if (auto b = LIEF::ELF::Parser::parse(raw)) {
+  auto stream = std::make_unique<LIEF::SpanStream>(data, size);
+  if (auto b = LIEF::ELF::Parser::parse(std::move(stream))) {
     std::stringstream oss;
     oss << *b;
   }
