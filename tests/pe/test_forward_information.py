644b17d47303ab731ddd95dcc2b13ad78946d857
Romain Thomas <me@romainthomas.fr>
2025-02-22 07:00:49 +0100

    Resolve #1168

--- tests/pe/test_forward_information.py
+++ tests/pe/test_forward_information.py
@@ -1,13 +1,10 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import lief
 import json
 from utils import get_sample
 
 def test_basic():
     path = get_sample('PE/PE32_x86_library_kernel32.dll')
-    sample = lief.parse(path)
+    sample = lief.PE.parse(path)
     exports = sample.get_export()
     forwarded_exports: list[lief.PE.ExportEntry] = [exp for exp in exports.entries if exp.is_forwarded]
     assert len(forwarded_exports) == 82
@@ -26,3 +23,11 @@ def test_basic():
     assert "forward_information" in json_serialized
     assert json_serialized["forward_information"]["library"] == "NTDLL"
     assert json_serialized["forward_information"]["function"] == "RtlInterlockedPushListSList"
+
+def test_issue_1168():
+    input_path = get_sample('PE/user32.dll')
+    pe = lief.PE.parse(input_path)
+    fwd = [e for e in pe.get_export().entries if e.is_forwarded]
+    assert len(fwd) == 4
+    assert fwd[0].name == "DefDlgProcA"
+    assert fwd[3].name == "DefWindowProcW"
