e1b54248b0ef7eccebdf5777b81309c448bcfecc
Romain Thomas <me@romainthomas.fr>
2025-07-13 21:22:06 +0200

    Fix casting error

--- api/rust/cargo/lief/src/elf/segment.rs
+++ api/rust/cargo/lief/src/elf/segment.rs
@@ -314,7 +314,7 @@ impl Segment<'_> {
 
     /// Fill the content of this segment with the value provided in parameter
     pub fn fill(&mut self, value: char) {
-        self.ptr.pin_mut().fill(value as i8)
+        self.ptr.pin_mut().fill((value as i8).try_into().unwrap())
     }
 }
 
