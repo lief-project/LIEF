188a1231a0100de5ac24bdff833f8991af455129
Romain Thomas <me@romainthomas.fr>
2026-02-15 12:46:33 +0100

    Add `LIEF_DOC_MINIFY` to driver the doc minification

--- doc/sphinx/sphinx_lief_doc/config/html_config.py
+++ doc/sphinx/sphinx_lief_doc/config/html_config.py
@@ -1,14 +1,17 @@
 import lief
+import os
 import sphinx_lief
 
 from sphinx.application import Sphinx
 
 def minify_option() -> dict[str, bool]:
-    #return {
-    #    "html_minify": True,
-    #    "css_minify": True,
-    #    "html_prettify": False,
-    #}
+    if os.getenv('LIEF_DOC_MINIFY') is not None:
+        return {
+            "html_minify": True,
+            "css_minify": True,
+            "html_prettify": False,
+        }
+
     return {
         "html_minify": False,
         "css_minify": False,
