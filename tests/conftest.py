ce04422edaa51aa4116a4abd2cc3ffefaadffd0d
Romain Thomas <me@romainthomas.fr>
2026-04-04 11:01:04 +0200

    tests: add `private` marker

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,15 @@
 Module containing configuration functions for Pytest.
 """
 import pytest
+from utils import has_private_samples
 
 def pytest_configure(config):
     # See https://docs.pytest.org/en/stable/how-to/mark.html
     config.addinivalue_line(
         "markers", "slow: mark test as slow."
+    )
+    config.addinivalue_line(
+        "markers", "private: the test needs private samples."
     )
 
 def pytest_addoption(parser):
@@ -16,7 +20,17 @@ def pytest_addoption(parser):
         help="Skip slow tests",
     )
 
+    parser.addoption(
+        "--skip-private",
+        action="store_true",
+        help="Skip private tests",
+    )
+
 def pytest_runtest_setup(item: pytest.Function):
     if item.get_closest_marker('slow') is not None:
         if item.config.getoption('--skip-slow'):
             pytest.skip("Skipping slow tests")
+
+    if item.get_closest_marker('private') is not None:
+        if item.config.getoption('--skip-private') or not has_private_samples():
+            pytest.skip("Skipping private test")
