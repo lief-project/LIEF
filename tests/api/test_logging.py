bc1a98bb3b3a11ef6c20e0e6758819971ee44cfd
Romain Thomas <me@romainthomas.fr>
2026-04-25 05:47:44 +0200

    tests: add `pytest-run-parallel` and mark thread-unsafe tests
    related to #1255

--- tests/api/test_logging.py
+++ tests/api/test_logging.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 
 import lief
+import pytest
 
 
 def _remove_eol(string: str):
     return string.replace("\n", "").replace("\r", "")
 
 
+@pytest.mark.thread_unsafe
 def test_filesink(tmp_path: Path):
     lief.logging.log(lief.logging.LEVEL.ERROR, "hello from default sink")
     out_log = tmp_path / "lief.log"
@@ -16,6 +18,7 @@ def test_filesink(tmp_path: Path):
     lief.logging.reset()
 
 
+@pytest.mark.thread_unsafe
 def test_stderr(capsys):
     lief.logging.log(lief.logging.LEVEL.ERROR, "This is an error")
     lief.logging.log(lief.logging.LEVEL.ERROR, "This is another error")
@@ -24,6 +27,7 @@ def test_stderr(capsys):
     assert _remove_eol(captured.err) == "This is an errorThis is another error"
 
 
+@pytest.mark.thread_unsafe
 def test_context_manager(capsys):
     lief.logging.reset()
     lief.logging.set_level(lief.logging.LEVEL.ERROR)
