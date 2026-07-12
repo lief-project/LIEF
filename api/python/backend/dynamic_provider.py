e1ff24cbc3f03cbe3aa5998e0edc031df206e7f0
Romain Thomas <me@romainthomas.fr>
2026-07-12 18:08:52 +0200

    python: move to [[tool.dynamic-metadata]]

--- api/python/backend/dynamic_provider.py
+++ api/python/backend/dynamic_provider.py
@@ -1,15 +1,20 @@
 from __future__ import annotations
 
+from collections.abc import Mapping
+from typing import Any
+
 from scikit_build_core._logging import rich_print
 from setup import BINDING_DIR
 from versioning import Versioning
 
 
-def dynamic_metadata(field, config) -> str | None:
-    if field == "version":
-        version = Versioning().get_version()
-        rich_print("{green}LIEF Version: {version}", version=version)
-        return version
-
-    if field == "readme":
-        return (BINDING_DIR / "README.rst").as_posix()
+def dynamic_metadata(
+    settings: Mapping[str, Any],
+    project: Mapping[str, Any],
+) -> dict[str, Any]:
+    version = Versioning().get_version()
+    rich_print("{green}LIEF Version: {version}", version=version)
+    return {
+        "version": version,
+        "readme": (BINDING_DIR / "README.rst").as_posix(),
+    }
