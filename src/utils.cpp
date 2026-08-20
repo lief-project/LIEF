5916c8d2b9b500ddd020d3e19fc7ab64bba35134
lief-agent <github@lief.re>
2026-08-20 15:39:32 +0200

    lint: reorder includes

--- src/utils.cpp
+++ src/utils.cpp
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <cstdlib>
 #include <algorithm>
 #include <cstddef>
+#include <cstdlib>
 #include <iterator>
 #include <string>
 
