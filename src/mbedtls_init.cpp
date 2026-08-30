7bf1f953186c7212e041a86c01a7cd20b91bc611
Romain Thomas <me@romainthomas.fr>
2026-08-30 15:08:49 +0200

    core: avoid pthread dependency in mbedtls initialization

--- src/mbedtls_init.cpp
+++ src/mbedtls_init.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <mutex>
+#include <cstdlib>
 
 #include "psa/crypto.h"
 #include "threading_alt.h"
@@ -31,13 +31,13 @@
 
 namespace LIEF {
 void mbedtls_init() {
-  static std::once_flag ONCE;
-  std::call_once(ONCE, [] {
+  [[maybe_unused]] static bool INITIALIZED = [] {
     mbedtls_init_threading_alt();
     if (psa_status_t status = psa_crypto_init(); status != PSA_SUCCESS) {
       LIEF_WARN("psa_crypto_init() didn't succeed: {}", (int)status);
     }
     std::atexit(mbedtls_psa_crypto_free);
-  });
+    return true;
+  }();
 }
 }
